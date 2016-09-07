3a74adfe41ce33895475ad8aa415ec9bee864d09
Rohan Sahai <rohan@weebly.com>
2016-09-06 23:07:12 -0700

    adds omni auth handling and session code slash controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'session/create'
+
+  get 'session/destroy'
+
+  get 'home/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
