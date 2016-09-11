6608f909cbe07090fffbde41a14b656438b2daad
Rohan Sahai <rohan@weebly.com>
2016-09-11 01:09:17 -0400

    adds method to determine winner pick location

--- app/assets/javascripts/components/game.es6.jsx
+++ app/assets/javascripts/components/game.es6.jsx
@@ -1,12 +1,24 @@
 class Game extends React.Component {
     render () {
         return (
-            <tr className={this.props.game.picked_team_location} onClick={this.handlePickSelect.bind(this)}>
-                <td> {this.props.game.home_team.name} </td>
-                <td> {this.props.game.away_team.name} </td>
+            <tr className={this.getPickedTeamLocation()} onClick={this.handlePickSelect.bind(this)}>
+                <td data-team-id={this.props.game.home_team.id}> {this.props.game.home_team.name} </td>
+                <td data-team-id={this.props.game.away_team.id}> {this.props.game.away_team.name} </td>
                 <td> {this.props.game.spread} </td>
             </tr>
         );
+    }
+
+    getPickedTeamLocation () {
+        if (!this.props.game.winner_id) {
+            return '';
+        }
+
+        if (this.props.game.home_team.id == this.props.game.winner_id) {
+            return 'home';
+        } else {
+            return 'away';
+        }
     }
 
     handlePickSelect (e) {
