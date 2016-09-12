2f264a09c66ac91ec1ec57ced90c937bd2ddf3b0
Rohan Sahai <rohan@weebly.com>
2016-09-11 20:53:51 -0400

    adds validators for max picks and time of pick on pick clasS

--- app/models/pick.rb
+++ app/models/pick.rb
@@ -2,4 +2,23 @@ class Pick < ApplicationRecord
   belongs_to :user
   belongs_to :game
   belongs_to :winner, class_name: "Team"
+
+  # validate :pick_locked, :on => :create
+  # validate :max_picks, :on => :create
+
+  MAX_PICKS = 5
+
+  private
+
+  def max_picks
+    if self.user.picks.length > MAX_PICKS
+      errors.add(:pick, "Already reached 5 picks")
+    end
+  end
+
+  def pick_locked
+    if self.game.time < Time.now
+      errors.add(:pick, "Game has already started")
+    end
+  end
 end
