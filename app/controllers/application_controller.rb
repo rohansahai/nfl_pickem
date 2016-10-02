47e76b5a8a8bcfb242c9bad6a24be584d9bde274
Rohan Sahai <rohan@weebly.com>
2016-10-02 15:13:46 -0700

    adds helper method to retrieve current week

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user
+  helper_method :current_user, :current_week
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def current_week
+    # get the earliest game with no result, that should be the active week
+    Game.where(:result => nil).order("time ASC").limit(1).first.week
   end
 end
