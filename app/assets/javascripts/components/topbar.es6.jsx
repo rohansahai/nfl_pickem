809f18fb4f9dc29b97bd8c59f4f95359c8880565
Rohan Sahai <rohan@weebly.com>
2016-10-02 15:13:18 -0700

    adds sign in sign out logic to topbar

--- app/assets/javascripts/components/topbar.es6.jsx
+++ app/assets/javascripts/components/topbar.es6.jsx
@@ -1,11 +1,18 @@
 class Topbar extends React.Component {
     render () {
+        if (this.props.current_user) {
+            var session_link = '/signout';
+            var session_link_text = 'Sign Out';
+        } else {
+            var session_link = '/auth/google_oauth2';
+            var session_link_text = 'Sign In';
+        }
         return (
             <nav>
                 <div className="nav-wrapper">
                     <a href="#" className="brand-logo title">Pick'em</a>
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
-                        <li><a href="/signout">Sign Out</a></li>
+                        <li><a href={session_link}>{session_link_text}</a></li>
                     </ul>
                 </div>
             </nav>
