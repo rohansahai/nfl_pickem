b542cbaf8a36f13202560b0d4de4bd778d9eacb6
Rohan Sahai <rohan@weebly.com>
2016-10-06 15:59:55 -0700

    adds uniqueness validation to games

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,9 @@
 class Game < ApplicationRecord
   belongs_to :away_team, class_name: "Team"
   belongs_to :home_team, class_name: "Team"
+
+  validates :week, uniqueness: { scope: :home_team_id,
+    message: "should only exist once per week" }
+  validates :week, uniqueness: { scope: :away_team_id,
+    message: "should only exist once per week" }
 end
