c0991b4ed6009f3764b24fd6a762e9c4556b64c4
Rohan Sahai <rohan@weebly.com>
2016-10-15 11:43:19 -0700

    adds previous picks url on a per user basis

--- app/assets/javascripts/components/user_record.es6.jsx
+++ app/assets/javascripts/components/user_record.es6.jsx
@@ -1,9 +1,10 @@
 class UserRecord extends React.Component {
     render () {
+        var previous_picks_url = "/users/" + this.props.user.id + "/picks";
         return (
             <tr className={(window.current_user.id === this.props.user.id) ? 'current-user' : ''}>
                 <td className="user-ranking"> {this.props.user.rank} </td>
-                <td> {this.props.user.name} </td>
+                <td> <a href={previous_picks_url}> {this.props.user.name} </a> </td>
                 <td> {this.props.user.wins} </td>
                 <td> {this.props.user.losses} </td>
                 <td> {this.props.user.draws} </td>
