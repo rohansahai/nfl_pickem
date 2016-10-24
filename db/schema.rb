1a81d5f332d6e29802cf7e19f802856f88ee9ec9
Rohan Sahai <rohan@weebly.com>
2016-10-23 23:56:51 -0700

    adds phone number to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161009002737) do
+ActiveRecord::Schema.define(version: 20161024065534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20161009002737) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.string   "phone_number"
   end
 
 end
