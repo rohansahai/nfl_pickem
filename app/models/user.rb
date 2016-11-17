79c1b3bc261de128a7b106dc878b8db60dd180f9
Rohan Sahai <rohan@weebly.com>
2016-11-17 08:24:04 -0500

    updates rake task and collects user emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
+      user.email = auth.info.email
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.save!
