118aaa4071001e5c40f17f199a9bf3f82ada9358
shaunchaudhary <shaun.chaudhary@bettercloud.com>
2017-09-14 20:38:42 -0400

    resize dynamically

--- app/helpers/weeks_helper.rb
+++ app/helpers/weeks_helper.rb
@@ -3,7 +3,7 @@ module WeeksHelper
     # bar_chart Pick.joins(:winner).where(:week =>
     # @current_week).group(:name).count.sort_by {|k, v| v}.reverse.to_h,
     bar_chart distribution_charts_path,
-    height: '1000px', width: '800px',
+    width: '800px',
      library: {
       yAxis: {
          allowDecimals: false,
