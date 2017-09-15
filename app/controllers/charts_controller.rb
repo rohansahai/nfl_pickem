b6c9649bb2a52220c9d045ce4bc9af74549c30da
shaunchaudhary <shaun.chaudhary@bettercloud.com>
2017-09-15 09:47:04 -0400

    change bars to not double count in distribution

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,19 +1,23 @@
 class ChartsController < ApplicationController
   def distribution
     distribution_hash = {}
-    picks = Pick.joins(:game).where(:week => current_week).where("games.time < ?", Time.now)
+    picks = Pick.joins(:game).where(:week => 1).where("games.time < ?", Time.now)
     results = picks.group(:result).count
-    colors = []
-    res_colors = {"win" => "green", "loss" => "red", "no result" => "blue"}
     results.each do |res, count|
       results_hash = {}
-      weekly_distro = picks.where(:result => res)
-      weekly_distro = picks.group(:game_id, :winner_id).count.sort_by {|k, v| v}.reverse.to_h
+      # weekly_distro = picks.where(:result => res)
+      weekly_distro = picks.group(:game_id, :winner_id).where(:result => res).count.sort_by {|k, v| v}.reverse.to_h
       weekly_distro.each do |key, pick_count|
         pick = Pick.find_by(:winner_id => key[1])
         winning_team = Team.find(key[1]).name
         game = Game.find(key[0])
-        # res = pick.result
+        if key[1] == game.spread_winner_id
+          res = "win"
+        elsif game.spread_winner_id == nil
+          res = "no result"
+        else
+          res = "loss"
+        end
 
         if game.home_team_id == pick.winner_id
           spread = game.home_spread
@@ -24,14 +28,9 @@ class ChartsController < ApplicationController
           spread = "+#{spread}"
         end
         labels = "#{winning_team} (#{spread})"
-        # results_hash[labels] = pick_count
-        if res == nil
-          res = 'no result'
-        end
-        colors << res_colors[res]
         results_hash[labels] = pick_count
+        distribution_hash[res] = results_hash
       end
-      distribution_hash[res] = results_hash
     end
     render json: [
       {name: "Win", data: distribution_hash["win"]},
