b485369e4a93347e984481473937dcd7829f92ae
shaunchaudhary <shaun.chaudhary@bettercloud.com>
2017-09-20 22:33:28 -0400

    add new week_standings function to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -57,6 +57,34 @@ class User < ApplicationRecord
     end
   end
 
+  def week_standings(week)
+    week_standings = {}
+    # @weeks_standings = Hash.new
+    week.downto(1).each do |this_week|
+          # @week_standings[this_week] = []
+          wins = picks.where(:result => 'win').where(:week => this_week).count
+          pushes = picks.where(:result => 'push').where(:week => this_week).count
+          losses = picks.where(:result => 'loss').where(:week => this_week).count
+          points = wins + (pushes * 0.5)
+          total = wins + losses + pushes
+          if total > 0
+            perc = points  / total * 100
+            perc.round()
+          else
+            perc = 0
+          end
+
+          week_standings[this_week] = {
+            :wins => wins,
+            :pushes => pushes,
+            :losses => losses,
+            :points => points,
+            :perc => perc
+          }
+    end
+    week_standings
+  end
+
   def get_picks_summary(week)
     weekly_picks = picks.where(:week => week)
     if weekly_picks.count < 1
