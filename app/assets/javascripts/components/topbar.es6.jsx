3207304c88168a82f62c1e9d7af2757913e20db2
“aroncey” <ssh-keygen -t rsa -C arthurroncey@gmail.com>
2017-09-22 14:34:09 -0400

    style and logo

--- app/assets/javascripts/components/topbar.es6.jsx
+++ app/assets/javascripts/components/topbar.es6.jsx
@@ -3,7 +3,9 @@ class Topbar extends React.Component {
         return (
             <nav>
                 <div className="nav-wrapper">
-                  <img className="superPickemLogo" src='http://aroncey.com/nfl_pickemL/images/superpickem.png'></img>
+                  <a href="http://www.superpickem.co/picks">                    
+                    <img className="superPickemLogo" src='http://aroncey.com/nfl_pickemL/images/superpickem.png'></img>
+                  </a>
                     <a href="#" className="brand-logo title"></a>
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
                         <li><a href="/signout">Sign Out</a></li>
