cb1c313edfab80efddebfa4184c08c11a8fa16b3
shaunchaudhary <shaun.chaudhary@bettercloud.com>
2017-10-17 14:02:48 -0400

    error handling to debug why prod str isnt working

--- lib/scripts/nfl_scores.py
+++ lib/scripts/nfl_scores.py
@@ -25,7 +25,7 @@ def is_there_game_on(current_week, prod_str):
     try:
         games['game_happening'] = games.apply(lambda row: 1 if row['start_time'] <= now <= row['end_time'] else 0, axis=1)
     except ValueError:
-        print(prod_str)
+        raise Exception(prod_str)
 
     return 1 if any(games['game_happening']) else 0
 
