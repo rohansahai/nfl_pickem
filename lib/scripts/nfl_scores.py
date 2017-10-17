1f115a23aa68d8ea8ba019ecbce11e1572e51114
shaunchaudhary <shaun.chaudhary@bettercloud.com>
2017-10-17 14:26:46 -0400

    test scores script by hard coding week as 6

--- lib/scripts/nfl_scores.py
+++ lib/scripts/nfl_scores.py
@@ -278,6 +278,7 @@ def main():
 
     prod_str = create_engine(os.environ['ENGINE_STR'])
     current_week = get_nfl_week_num()
+    current_week = 6
     game_live = is_there_game_on(current_week, prod_str)
     if game_live:
         # prod_str = get_local_str()
