c13eed798fc08adcdae01dc8f5804790a48383b2
Rohan Sahai <rohan@sah.ai>
2019-08-11 14:25:37 -0700

    Revert "temp fixes for migrations"
    This reverts commit 3983e63bbe1691e298a6c7e538e0333af3f5f0b1.

--- db/migrate/20180902000623_rename_team_mapping_to_match_prod.rb
+++ db/migrate/20180902000623_rename_team_mapping_to_match_prod.rb
@@ -4,8 +4,8 @@ class RenameTeamMappingToMatchProd < ActiveRecord::Migration[5.0]
     remove_column :team_mappings, :created_at, :datetime
     remove_column :team_mappings, :updated_at, :datetime
     rename_table :team_mappings, :team_mapping
-    # add_column :teams, :logo_path, :string
-    # add_column :teams, :wins, :integer
-    # add_column :teams, :losses, :integer
+    add_column :teams, :logo_path, :string
+    add_column :teams, :wins, :integer
+    add_column :teams, :losses, :integer
   end
 end
