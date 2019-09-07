0d3bf48d243a533dd45f4a74ab3ffd6e875f1d8e
shaun3k <shaun@meetresident.com>
2019-09-07 12:49:51 -0400

    fix rake update results

--- lib/scripts/db.py
+++ lib/scripts/db.py
@@ -64,4 +64,16 @@ def get_ff_url(current_week):
 
 def get_vi_url(week):
 
-    return "http://www.vegasinsider.com/nfl/matchups/matchups.cfm/week/{}/season/2018".format(week)
+    return "http://www.vegasinsider.com/nfl/matchups/matchups.cfm/week/{}/season/2019".format(week)
+
+
+def get_gmail_creds():
+
+    return 'shaun.chaudhary@gmail.com', 'bjfwispetfyhzkxd'
+
+
+def get_local_str():
+
+    prod_str = 'postgresql+psycopg2://dtaxgwznvvlhde:6tiVAc1zqt3WKf4pfty52ti0Tw@ec2-54-243-217-22.compute-1.amazonaws.com:5432/deij2vlu5g0eh8'
+
+    return prod_str
